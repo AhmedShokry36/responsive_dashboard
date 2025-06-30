0acf85536dba1047b68fbf5ae1b0225243887ca5
AhmedShokry36 <32983552+AhmedShokry36@users.noreply.github.com>
2025-06-30 15:15:11 +0300

    Add DashboardView and update app entry point
    Introduced a new DashboardView widget and refactored main.dart to use DashboardView as the home screen. Updated widget_test.dart to reflect the new entry point.

---
+++ lib/View/DashboardView.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Dashboardview extends StatelessWidget {
+  const Dashboardview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(), body: Container(child: Text("data")));
+  }
+}
